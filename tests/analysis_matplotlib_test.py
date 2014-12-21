c2641cfcf13793e39ea552c4c75455406c1a86bb
Phillip Green II <phillip.green.ii@gmail.com>
2014-12-21 18:45:11 -0500

    conditionally run matplotlib tests only if available

--- tests/analysis_matplotlib_test.py
+++ tests/analysis_matplotlib_test.py
@@ -21,7 +21,16 @@ import loan_payoff_tools.payment_manager as payment_manager
 from loan_payoff_tools.money import Money
 from loan_payoff_tools.payment_manager import Account
 
-class AnalysisTestCase(unittest.TestCase):
+try:
+    import matplotlib.pyplot
+    matplotlib_available = True
+except ImportError:
+    matplotlib_available = False
+    pass
+
+
+@unittest.skipUnless(matplotlib_available, "matplotlib not available")
+class AnalysisMatplotlibTestCase(unittest.TestCase):
     def setUp(self):
         self.accounts = analysis.load_accounts(os.path.join('tests', 'data','test-accounts.csv'))
         self.temp_dir = tempfile.mkdtemp('analysis-test')
@@ -29,22 +38,7 @@ class AnalysisTestCase(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.temp_dir)
 
-    def test_analyze(self):
-        mpd = max_payment_determiner.ConstantMaxPaymentDeterminer(2000, 50)
-        pm = payment_manager.BiggestDebtPaymentManager()
-        bpm = payment_manager.EvenSplitPaymentManager()
-
-        (r_mpd, r_pm, r_bpm, months, initial_paid, total_paid, to_interest, monthly_payments) = analysis.analyze(mpd, pm, bpm, self.accounts)
-        self.assertEqual(r_mpd, mpd)
-        self.assertEqual(r_pm, pm)
-        self.assertEqual(r_bpm, bpm)
-        self.assertEqual(months, 75)
-        self.assertEqual(initial_paid, Money(133000.00))
-        self.assertEqual(total_paid, Money(152349.04))
-        self.assertEqual(to_interest, Money(19349.04))
-        self.assertIsNotNone(monthly_payments)
-
-    def test_dump_monthly_payments_to_csv(self):
+    def test_dump_monthly_payments_to_png(self):
         account0 = Account("Bank1", "00", "Person2", 5000.00, 0.05, 50.00, datetime.date(2014, 10, 7))
         account1 = Account("Bank1", "01", "Person2", 3000.00, 0.04, 40.00, datetime.date(2014, 10, 7))
         account2 = Account("Bank1", "02", "Person2", 7000.00, 0.03, 100.00, datetime.date(2014, 10, 7))
@@ -60,10 +54,9 @@ class AnalysisTestCase(unittest.TestCase):
             (datetime.date(2015, 5, 6), {account2: (Money(14.92), Money(0.00)), account1: (Money(848.48), Money(0.00))})
             ]
 
-        output_file = os.path.join(self.temp_dir, 'test.csv')
-        analysis.dump_monthly_payments_to_csv(output_file, monthly_payments)
+        output_file = os.path.join(self.temp_dir, 'test.png')
+        analysis.dump_monthly_payments_to_png(output_file, monthly_payments)
         self.assertTrue(os.path.isfile(output_file))
-        self.assertEqual(open(output_file).readlines(), open(os.path.join('tests', 'data', 'expected-test-results-dump.csv')).readlines())
 
 if __name__ == '__main__':
     unittest.main()
